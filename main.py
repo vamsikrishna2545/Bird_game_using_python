5658689d2eeacf2c1561e87c9473081a6f90838e
vamsikrishna2545 <114431853+vamsikrishna2545@users.noreply.github.com>
2023-06-28 12:41:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
-import pyttsx3
-import speech_recognition as sr
-def take_commands():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('Listening')
-        r.pause_threshold = 0.7
-        audio = r.listen(source)
-        try:
-            print("Recognizing")
-            Query = r.recognize_google(audio)
-            print("the query is printed='", Query, "'")
-        except Exception as e:
-            print(e)
-            print("Say that again sir")
-            return "None"
-    return Query
-def Speak(audio):
-    engine = pyttsx3.init()
-    engine.say(audio)
-    engine.runAndWait()
-if __name__ == '__main__':
-    while True:
-        command = take_commands()
-        if "exit" in command:
-            Speak("Sure sir! as your wish, bai")
-            break
-        if "insta" in command:
-            Speak("Best python page on instagram is pythonhub")
-        if "learn" in command:
-            Speak("copyassignment website is best to learn python")
-        if "code" in command:
-            Speak("You can get this code from copyassignment website")
+import random
+import sys 
+import pygame
+from pygame.locals import *
+
+FPS = 32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT*0.8
+GAME_SPRITES = {}
+GAME_SOUNDS = {}
+PLAYER = 'resources\SPRITES\\bird.png'
+BACKGROUND = 'resources\SPRITES\\bg.jpeg'
+PIPE = 'resources\SPRITES\pipe.png '
+if __name__ == "__main__":
+
+    pygame.init() 
+    FPSCLOCK = pygame.time.Clock() 
+    pygame.display.set_caption('Flappy Bird With Sameer') 
+
+    GAME_SPRITES['numbers'] = (
+        pygame.image.load('resources\SPRITES\\0.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\1.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\2.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\3.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\4.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\5.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\6.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\7.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\8.png').convert_alpha(),
+        pygame.image.load('resources\SPRITES\\9.png').convert_alpha(),
+    
+    ) 
+    S
+    
+    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert_alpha()
+    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
+    GAME_SPRITES['message'] = pygame.image.load('resources\SPRITES\message.png').convert_alpha()
+    GAME_SPRITES['base'] = pygame.image.load('resources\SPRITES\\base.png').convert_alpha()
+    GAME_SPRITES['pipe'] = (
+        
+        
+    pygame.transform.rotate(pygame.image.load(PIPE).convert_alpha(), 180),
+    pygame.image.load(PIPE).convert_alpha()  
+    )
+    GAME_SOUNDS['die'] = pygame.mixer.Sound('resources\AUDIO\die.wav')
+    GAME_SOUNDS['hit'] = pygame.mixer.Sound('resources\AUDIO\hit.wav')
+    GAME_SOUNDS['point'] = pygame.mixer.Sound('resources\AUDIO\point.wav')
+    GAME_SOUNDS['swoosh'] = pygame.mixer.Sound('resources\AUDIO\swoosh.wav')
+    GAME_SOUNDS['wing'] = pygame.mixer.Sound('resources\AUDIO\wing.wav')
